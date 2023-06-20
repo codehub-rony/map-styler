06d7b36d8cdb8aa3da9bf9159d55e09492d5dbac
unknown <rbnedkov@gmail.com>
2023-06-20 21:27:24 +0200

    Added footbar and change primary color

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-footer absolute class="pa-0 d-flex justify-center app-footer">
+    <div class="d-flex align-center">
+      <div class="text-center text-caption mt-1" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Rony Nedkov</strong>
+      </div>
+    </div>
+  </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
