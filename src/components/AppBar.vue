8908b784360bc72b6d23cd52d7603720004e4433
unknown <rbnedkov@gmail.com>
2023-06-21 22:14:04 +0200

    Added buy me a coffee and reload page on logo click

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,11 +1,41 @@
 <template>
   <v-app-bar flat>
-    <v-container class="fill-height d-flex align-center">
+    <div class="d-flex align-center">
+      <v-img
+        src="@/assets/asset 3.svg"
+        width="50px"
+        class="pa-2 ml-2 app-bar-logo"
+        @click="refresh"
+      />
+    </div>
+    <v-container class="fill-height d-flex align-center ma-0">
       <v-app-bar-title>Vectortile Style creator</v-app-bar-title>
 
       <v-spacer></v-spacer>
+      <v-btn
+        color="#FFDD00"
+        size="small"
+        variant="flat"
+        href="https://www.buymeacoffee.com/RonyNedkov"
+        target="_blank"
+        ><span> &#9749; </span> Buy me a coffee</v-btn
+      >
     </v-container>
   </v-app-bar>
 </template>
 
-<script setup></script>
+<script>
+export default {
+  methods: {
+    refresh: function () {
+      window.location.href = "/";
+    },
+  },
+};
+</script>
+
+<style>
+.app-bar-logo:hover {
+  cursor: pointer;
+}
+</style>
