a50c76c72004562251dcc288e48514389623b67a
unknown <rbnedkov@gmail.com>
2023-07-07 17:37:52 +0200

    update default style

--- src/utils/CircleLayer.js
+++ src/utils/CircleLayer.js
@@ -6,25 +6,25 @@ class CircleLayer extends BaseLayer {
     this.attributes = [
       {
         name: "circle-color",
-        value: { r: 255, g: 255, b: 255, a: 0.5 },
-        component: { label: "point color", type: "color_picker" },
+        value: { r: 232, g: 227, b: 223, a: 0.7 },
+        component: { label: "color", type: "color_picker" },
       },
 
       {
         name: "circle-radius",
-        value: 3,
-        component: { label: " point radius", type: "input_field" },
+        value: 4,
+        component: { label: "radius", type: "input_field" },
       },
 
       {
         name: "circle-stroke-color",
-        value: { r: 255, g: 255, b: 255, a: 0.5 },
+        value: { r: 54, g: 154, b: 204, a: 1 },
         component: { label: "stroke color", type: "color_picker" },
       },
 
       {
         name: "circle-stroke-width",
-        value: 3,
+        value: 1,
         component: { label: "stroke width", type: "input_field" },
       },
     ];
