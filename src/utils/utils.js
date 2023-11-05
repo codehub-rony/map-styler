d75f0f07fcdec766c09e12e14cd29fbda68f28a6
Rony <rbnedkov@gmail.com>
2023-11-05 20:03:50 +0100

    init utils.js with generateId method for unique layer ID's

---
+++ src/utils/utils.js
@@ -0,0 +1,16 @@
+const generateId = function () {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < 10) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+};
+
+export default {
+  generateId,
+};
