da5edac0598d40ee64cd9046b53b47d79914508b
Rony <rbnedkov@gmail.com>
2023-11-05 20:06:18 +0100

    init components for filter creation #9

---
+++ src/components/Filters/AddFilterBtn.vue
@@ -0,0 +1,159 @@
+<template>
+  <div>
+  <div class="d-flex justify-center pb-3">
+    <v-btn
+      class="mt-2"
+      elevation="0"
+      variant="outlined"
+      rounded="0"
+      size="small"
+      @click="dialog = true"
+      >create filter
+    </v-btn>
+  </div>
+  <v-dialog persistent v-model="dialog" width="400">
+    <v-card>
+      <v-card-title class="text-h6 font-weight-light">New filter</v-card-title>
+      <v-card-text>
+        <div class="d-flex flex-column">
+          <span class="text-body-2 font-weight-bold pb-1">Label</span>
+          <v-text-field
+            v-model="label"
+            :error-messages="messages"
+            variant="outlined"
+            density="compact"
+            @update:modelValue="messages = []"
+          ></v-text-field>
+        </div>
+
+        <div
+          class="mt-4 text-body-2 font-weight-bold pb-1"
+          v-if="conditions.length > 0"
+        >
+          Conditions
+        </div>
+
+        <ConditionCreator
+          v-for="(condition, i) in conditions"
+          :key="i"
+          :filter="condition"
+          :attributes="attributes"
+          @update-condition="updateCondition"
+          @remove-condition="removeCondition"
+        />
+        <div class="d-flex justify-end mt-1">
+          <v-btn flat size="x-small" rounded="0" @click="createCondition"
+            >add condition</v-btn
+          >
+        </div>
+      </v-card-text>
+      <v-divider class="mt-3"></v-divider>
+      <div class="d-flex flex-row justify-center pa-4">
+        <v-btn variant="text" size="small" rounded="0" @click="dialog = false"
+          >cancel</v-btn
+        >
+        <v-btn
+          color="primary"
+          size="small"
+          flat
+          rounded="0"
+          @click="create()"
+          >create</v-btn
+        >
+      </div>
+    </v-card>
+  </v-dialog>
+  </div>
+</template>
+
+<script>
+import InputField from "@/components/LayerList/InputField.vue";
+import ColorField from "@/components/LayerList/ColorField.vue";
+import ConditionCreator from "./ConditionCreator.vue";
+
+// utils
+import utils from "@/utils/utils.js";
+
+export default {
+  components: {
+    InputField,
+    ColorField,
+    ConditionCreator,
+  },
+  props: {
+    styleObject: Object,
+  },
+  data() {
+    return {
+      isClicked: 0,
+      dialog: false,
+      label: null,
+      messages: [],
+      conditions: [],
+      attributes: [],
+    };
+  },
+  mounted() {
+    this.attributes = this.styleObject.getFeatureAttributes();
+    this.createCondition(Object.keys(this.attributes)[0]);
+  },
+  methods: {
+    createCondition: function (attribute) {
+      let filter = { id: 0, condition: ["==", attribute, null] };
+
+      if (this.conditions.length > 0) {
+        filter.id = this.conditions[this.conditions.length - 1].id + 1;
+        filter.condition[1] =
+          this.conditions[this.conditions.length - 1].condition[1];
+      }
+
+      this.conditions.push(filter);
+    },
+    updateCondition: function (update) {
+      let index = this.conditions.findIndex((x) => x.id === update.id);
+      this.conditions[index].condition = update.condition;
+    },
+    removeCondition: function (condition) {
+      this.conditions.forEach((item, i) => {
+        if (item.id === condition.id) {
+          this.conditions.splice(i, 1);
+        }
+      });
+    },
+    createFilterArray: function () {
+      let filter = ["all"];
+
+      this.conditions.forEach((condition) => {
+        filter.push(condition.condition);
+      });
+
+      return filter
+    },
+    create: function () {
+      if (this.label) {
+        let filter = this.createFilterArray();
+        let layer_id = utils.generateId();
+
+        this.styleObject.addFilter(layer_id, this.label, filter);
+        this.closeAndResetDialog()
+      } else {
+        this.messages.push('required field')
+      }
+    },
+    closeAndResetDialog: function () {
+      this.conditions = [{ id: 0, condition: ["==", "bouwjaar", null] }];
+      this.label = ''
+      this.dialog = false;
+    },
+
+    deleteLayer: function (layer) {
+      this.styleObject.layers.forEach((item, i) => {
+        if (item.id === layer.id) {
+          this.styleObject.layers.splice(i, 1);
+        }
+      });
+    },
+  },
+};
+</script>
+<style></style>
---
+++ src/components/Filters/ConditionCreator.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="d-flex flex-column pb-1">
+    <div class="d-flex flex-row">
+      <DeleteButton @click="handleEvent" />
+      <select v-model="attribute" class="add-filter-input-select">
+        <option
+          :value="attribute"
+          v-for="(attribute, i) in properties"
+          :key="i"
+        >
+          {{ attribute }}
+        </option>
+      </select>
+
+      <select class="custom-select" v-model="operator">
+        <option
+          class="custom-select-option"
+          :value="operator"
+          v-for="(operator, i) in operators"
+          :key="i"
+        >
+          {{ operator }}
+        </option>
+      </select>
+
+      <input class="add-filter-input-select" id='filter-input-type' v-model="value" />
+    </div>
+  </div>
+</template>
+<script>
+import DeleteButton from "@/components/DeleteButton.vue";
+
+export default {
+  emits: ["update-condition", "remove-condition"],
+  components: {
+    DeleteButton,
+  },
+  props: {
+    filter: Object,
+    attributes: Object,
+  },
+  data() {
+    return {
+      attribute: null,
+      properties: [],
+      operator: "==",
+      value: null,
+      operators: ["==", ">=", "<=", "<", ">"],
+    };
+  },
+  mounted() {
+    this.properties = Object.keys(this.attributes)
+
+  },
+  unmounted() {
+    this.attribute = '';
+    this.operator = '';
+    this.value = '';
+  },
+  methods: {
+    handleEvent: function () {
+      this.$emit("remove-condition", this.filter);
+    },
+    parseIntIfNumber: function() {
+      return this.attributes[this.attribute] === 'number' ? parseInt(this.value) : this.value
+
+    },
+    updateFilter: function() {
+      let value = this.parseIntIfNumber()
+      this.$emit("update-condition", {
+        id: this.filter.id,
+        condition: [this.operator, this.attribute, value],
+      });
+
+    }
+  },
+  watch: {
+    attribute: function () {    
+      this.updateFilter()
+    },
+    operator: function () {
+      this.updateFilter()
+    },
+    value: function () {
+      this.updateFilter()
+
+    },
+  },
+};
+</script>
+
+<style>
+.add-filter-input-select {
+  width: 100%;
+  padding: 4px 10px 4px 10px;
+  font-size: small;
+  border-radius: 0px;
+  border: 1px solid lightgrey;
+}
+.custom-select {
+  padding: 4px 10px 4px 10px;
+  border: 1px solid lightgrey;
+  font-size: small;
+  color: #1c87c9;
+  border-radius: 0px;
+}
+
+.custom-select-option {
+  border-radius: 0px;
+}
+</style>
