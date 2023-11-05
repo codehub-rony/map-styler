c89668cfff6c8d8a197d4ab3f181150c46a18a07
Rony <rbnedkov@gmail.com>
2023-11-05 20:04:51 +0100

    init generic delete btn component. Improved styiing

---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-btn
+    variant="text"
+    icon="mdi-window-close"
+    color="grey"
+    size="x-small"
+    @click="handleClick"
+  ></v-btn>
+</template>
+<script>
+export default {
+  props: {
+    callback: Function,
+    item: Object,
+  },
+  methods: {
+    handleClick: function () {
+      if (this.callback) {
+        this.callback(this.item);
+      } else {
+        this.$emit("click");
+      }
+    },
+  },
+};
+</script>
--- src/components/LayerList/LayerList.vue
+++ src/components/LayerList/LayerList.vue
@@ -3,12 +3,18 @@
     <div class="pl-4 pb-1 pt-4 text-h6 font-weight-light">
       {{ styleObject.style_name }}
     </div>
-
     <div v-for="(layer, i) in styleObject.layers" :id="i" class="">
+      <div class="d-flex flex-row align-center  pr-3 pl-1">
+        <DeleteButton :callback="deleteLayer" :item="layer" class="mb-1" />
+        <span class="text-subtitle-2">{{ layer.label }}</span>
+
+
+      </div>
+
       <div
         v-for="(attribute, h) in layer.attributes"
         :id="h"
-        class="text-subtitle-2 pl-7 pr-4 pt-1 pb-2 d-flex flex-row justify-space-between"
+        class="text-subtitle-2 test pr-4 pb-2 d-flex flex-row justify-space-between"
       >
         <ColorField
           :attribute="attribute"
@@ -20,6 +26,7 @@
           v-if="attribute.component.type === 'input_field'"
         />
       </div>
+
       <v-divider class="pb-2" v-if="styleObject.layers.length - 1 !== i">
       </v-divider>
     </div>
@@ -29,17 +36,35 @@
 <script>
 import InputField from "./InputField.vue";
 import ColorField from "./ColorField.vue";
+import DeleteButton from '@/components/DeleteButton.vue';
 import { useAppStore } from "@/store/app.js";
 import { mapState } from "pinia";
 export default {
   components: {
     InputField,
     ColorField,
+    DeleteButton,
   },
   computed: {
     ...mapState(useAppStore, ["styleObject"]),
   },
+
+  methods: {
+    deleteLayer: function (layer) {
+      //to do add function in StyleObject for deleting layers
+      this.styleObject.layers.forEach((item, i) => {
+        if (item.id === layer.id) {
+          this.styleObject.layers.splice(i, 1);
+        }
+      });
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+.test {
+  margin-left: 36px;
+}
+
+</style>
