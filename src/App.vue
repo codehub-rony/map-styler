6f4bfc6c2d920e92eec61f183711ef5bafdd24e3
Rony Nedkov <rony.nedkov@ilent.nl>
2024-03-09 11:14:47 +0100

    disable grey background

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app id="app_container">
     <AppBar />
-    <v-main class="bg-grey-lighten-3">
+    <!-- <v-main class="bg-grey-lighten-3"> -->
+    <v-main>
       <router-view></router-view>
       <!-- <AppFooter /> -->
     </v-main>
