a1625911efb12980d4b3c7154bda986f332598c7
Rony Nedkov <rony.nedkov@ilent.nl>
2024-04-26 11:30:16 +0200

    Implemented click interaction with popup closes #6

--- src/components/MapViewer.vue
+++ src/components/MapViewer.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <v-sheet :height="height" id="map_container"> </v-sheet>
+    <MapPopup
+      :map="map"
+      :vectorLayer="vectorLayer"
+      :styleObject="styleObject"
+      v-if="map && styleObject"
+    />
   </div>
 </template>
 
@@ -10,15 +16,15 @@ import View from "ol/View.js";
 import { unByKey } from "ol/Observable.js";
 import * as olExtent from "ol/extent";
 
+// Components
+import MapPopup from "@/components/MapPopup.vue";
+
 // basemap
 import OSM from "ol/source/OSM.js";
 import TileLayer from "ol/layer/Tile.js";
 
 // Styling
 import { stylefunction } from "ol-mapbox-style";
-
-// Utils
-import SelectInteraction from "@/utils/mapviewer/SelectInteraction.js";
 
 // OGC Tile layer
 import VectorTileLayer from "ol/layer/VectorTile.js";
@@ -36,6 +42,9 @@ import { useAppStore } from "@/store/app.js";
 import { mapState } from "pinia";
 
 export default {
+  components: {
+    MapPopup,
+  },
   props: {
     geodataSource: Object,
   },
@@ -54,15 +63,15 @@ export default {
     this.setHeight();
     this.initMap();
 
-    // Redirect to landingpage on pagre reload
+    // Redirect to landingpage on page reload
     if (this.styleObject) {
       this.createVectorLayer();
     } else {
       this.$router.push("/");
     }
 
-    this.map.on("click", (event) => {
-      // console.log(evt.coordinate, evt.map.getView().getZoom());
+    this.map.on("click", (evt) => {
+      console.log(evt.coordinate, evt.map.getView().getZoom());
     });
   },
   methods: {
@@ -120,11 +129,6 @@ export default {
           }
         });
       }
-      new SelectInteraction(
-        this.map,
-        this.vectorLayer,
-        this.styleObject.source_type
-      );
     },
     zoomToExtent: function (extent) {
       let resolution = this.view.getResolutionForExtent(extent);
