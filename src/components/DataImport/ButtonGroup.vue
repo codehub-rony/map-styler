c9ff5063ff2c59d58b04421774c306419ee28941
Rony Nedkov <rony.nedkov@ilent.nl>
2024-07-11 12:29:39 +0200

    init generic component for dialog buttons

---
+++ src/components/DataImport/ButtonGroup.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="d-flex flex-row">
+    <v-btn flat @click="handleDecline" variant="text">
+      <slot name="decline-btn-label">cancel</slot></v-btn
+    >
+    <v-btn color="primary" flat @click="handleConfirm"
+      ><slot name="confirm-btn-label">import</slot></v-btn
+    >
+  </div>
+</template>
+<script>
+export default {
+  emits: ["confirm", "decline"],
+
+  methods: {
+    handleConfirm: function () {
+      this.$emit("confirm");
+    },
+    handleDecline: function () {
+      this.$emit("decline");
+    },
+  },
+};
+</script>
