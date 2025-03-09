7fde9c6edba01fc6bd0fa24b75ceb555f61cf32b
Rony Nedkov <rony.nedkov@ilent.nl>
2025-03-09 22:00:40 +0100

    fix missing slash

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const api_baseUrl = "http://localhost:8000/api/";
-const api_baseUrl = import.meta.env.VITE_BACKEND_URL;
+const api_baseUrl = `${import.meta.env.VITE_BACKEND_URL}/api/`;
 axios.interceptors.request.use(
   function (config) {
     const mapstyler_user = localStorage.getItem("mapstyler_user");
@@ -55,32 +55,32 @@ const requests = {
 
 const Project = {
   getAll: () => {
-    return requests.get(`${api_baseUrl}/project/project/`);
+    return requests.get(`${api_baseUrl}project/project/`);
   },
   create: (data) => {
-    return requests.post(`${api_baseUrl}/project/project/`, data);
+    return requests.post(`${api_baseUrl}project/project/`, data);
   },
   delete: (project_id) => {
-    return requests.delete(`${api_baseUrl}/project/project/${project_id}/`);
+    return requests.delete(`${api_baseUrl}project/project/${project_id}/`);
   },
   getStylejsons: (project_id) => {
-    return requests.get(`${api_baseUrl}/project/${project_id}/stylejsons/`);
+    return requests.get(`${api_baseUrl}project/${project_id}/stylejsons/`);
   },
   saveStyleJSON: (project_id, stylejson_id, payload) => {
     return requests.put(
-      `${api_baseUrl}/project/${project_id}/stylejsons/${stylejson_id}/`,
+      `${api_baseUrl}project/${project_id}/stylejsons/${stylejson_id}/`,
       payload
     );
   },
   createStyleJSON: (project_id, payload) => {
     return requests.post(
-      `${api_baseUrl}/project/${project_id}/stylejsons/`,
+      `${api_baseUrl}project/${project_id}/stylejsons/`,
       payload
     );
   },
   deleteStyleJSON: (project_id, stylejson_id) => {
     return requests.delete(
-      `${api_baseUrl}/project/${project_id}/stylejsons/${stylejson_id}/`
+      `${api_baseUrl}project/${project_id}/stylejsons/${stylejson_id}/`
     );
   },
 };
