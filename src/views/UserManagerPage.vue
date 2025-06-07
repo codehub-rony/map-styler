7d28c18a949a42e47fe26d3b6bd484e16f75ba72
Rony Nedkov <rony.nedkov@ilent.nl>
2025-06-07 11:42:29 +0200

    Finalized password reset UI

--- src/views/UserManagerPage.vue
+++ src/views/UserManagerPage.vue
@@ -12,33 +12,77 @@
         <InputTextField v-model="email" :disabled="true"
           ><template #label>Email</template></InputTextField
         >
-        <InputTextField v-model="name"
+        <InputTextField v-model="name" :disabled="true"
           ><template #label>name</template></InputTextField
         >
-
-        <div v-if="updatePassword">
+      </v-col>
+    </v-row>
+    <v-row class="justify-center" v-if="updatePassword">
+      <v-col cols="6"> <v-divider></v-divider> </v-col>
+    </v-row>
+    <v-row class="justify-center" v-if="updatePassword">
+      <v-col cols="6">
+        <v-form ref="passform" v-if="updatePassword">
           <InputTextField
-            ><template #label>Old password</template></InputTextField
+            v-model="password"
+            :type="'password'"
+            :validationRules="['not_empty']"
+            ><template #label>Current password</template></InputTextField
           >
           <InputTextField
+            v-model="newPassword"
+            :type="'password'"
+            :validationRules="['not_empty']"
             ><template #label>New password</template></InputTextField
           >
           <InputTextField
+            v-model="repeatPassword"
+            :type="'password'"
+            :validationRules="['not_empty']"
             ><template #label>Repeat new password</template></InputTextField
           >
-          <v-btn>save</v-btn>
-          <v-btn @click="updatePassword = false">cancel</v-btn>
+        </v-form>
+        <div class="d-flex justify-center">
+          <div
+            :style="{
+              visibility: errorMessage ? 'visible' : 'hidden',
+            }"
+            class="text-subtitle-2 error-message"
+          >
+            {{ errorMessage }}
+          </div>
         </div>
+        <div class="d-flex justify-end">
+          <v-btn
+            color="black"
+            rounded="0"
+            elevation="0"
+            class="mt-2"
+            variant="text"
+            @click="clearPasswordUpdate"
+            >cancel</v-btn
+          >
+          <v-btn
+            color="primary"
+            rounded="0"
+            elevation="0"
+            class="mt-2"
+            @click="handleUpdatePassword"
+            :loading="loading"
+            >save changes</v-btn
+          >
+        </div>
+      </v-col>
+    </v-row>
+    <v-row class="justify-center" v-if="!updatePassword">
+      <v-col cols="3">
         <div class="d-flex flex-column self-align-start">
           <v-btn
             variant="text"
-            class="text-subtitle-2"
+            class="text-subtitle-2 mt-5"
             @click="updatePassword = true"
             v-if="!updatePassword"
             >change password</v-btn
-          >
-          <v-btn variant="text" class="text-subtitle-2" color="red"
-            >delete account</v-btn
           >
         </div>
       </v-col>
@@ -58,6 +102,7 @@ import InputTextField from "@/components/GenericComponents/InputTextField.vue";
 import { useAuthStore } from "@/store/auth.js";
 import { update } from "lodash";
 import { mapActions, mapState } from "pinia";
+import apiService from "@/services/apiService";
 
 export default {
   components: {
@@ -69,6 +114,12 @@ export default {
       name: "test",
       email: null,
       updatePassword: false,
+      password: null,
+      newPassword: null,
+      repeatPassword: null,
+      loading: false,
+      errorMessage: null,
+      errorTimeout: null,
     };
   },
   computed: {
@@ -76,12 +127,59 @@ export default {
   },
   mounted() {
     this.email = this.user_email;
+
+    apiService.User.get().then((res) => {
+      this.name = res.name;
+    });
   },
+  methods: {
+    handleUpdatePassword: async function () {
+      const { valid } = await this.$refs.passform.validate();
+
+      if (valid) {
+        this.loading = true;
+        let payload = {
+          email: this.user_email,
+          name: this.name,
+          password: this.password,
+          new_password: this.newPassword,
+          confirm_new_password: this.repeatPassword,
+        };
+        apiService.User.update(payload)
+          .then((res) => {
+            this.clearPasswordUpdate();
+          })
+          .catch((err) => {
+            this.loading = false;
+            if (this.errorTimeout) {
+              clearTimeout(this.errorTimeout);
+              this.errorMessage = null;
+            }
+
+            if (err.response && err.response.data) {
+              const error = err.response.data;
+
+              this.errorMessage = error.error_msg;
+
+              this.errorTimeout = setTimeout(() => {
+                this.errorMessage = null;
+              }, 4000);
+            }
+          });
+      }
+    },
+    clearPasswordUpdate: function () {
+      this.updatePassword = this.loading = false;
+      this.password = this.newPassword = this.repeatPassword = "";
+    },
+  },
+  handleDelete: function () {},
 };
 </script>
 
-<!-- <InputTextField
-v-model="projectName"
-:validationRules="['not_empty']"
-ref="projectname"
-><template #label>Project name</template> -->
+<style>
+.error-message {
+  min-height: 1.3rem;
+  color: #b71734;
+}
+</style>
